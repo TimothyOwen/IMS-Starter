bfa27c2d5d0e2eb1e9cad0723ff577b11455c2a2
TimothyOwen <towen@qa.com>
2021-01-30 09:29:27 +0000

    Modified schema to match updated ERD

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -8,15 +8,12 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `customer_id` INT(11) NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(40) DEFAULT NULL,
     `surname` VARCHAR(40) DEFAULT NULL,
-    `shipping_address` VARCHAR(100) DEFAULT NULL,
-    `credit_card_no` VARCHAR(16) DEFAULT NULL,
     PRIMARY KEY (`customer_id`)
 );
 
 CREATE TABLE IF NOT EXISTS `ims`.`orders` (
     `order_id` INT(11) NOT NULL AUTO_INCREMENT,
     `customer_id` INT(11) DEFAULT NULL,
-    `order_item_id` INT(11) DEFAULT NULL,
     `cost` DECIMAL(7,2) DEFAULT NULL,
     `shipment_date` DECIMAL(7,2) DEFAULT NULL,
     PRIMARY KEY (`order_id`)
@@ -38,6 +35,5 @@ CREATE TABLE IF NOT EXISTS `ims`.`items` (
 );
 
 ALTER TABLE `ims`.`orders` ADD FOREIGN KEY (`customer_id`) REFERENCES `ims`.`customers` (`customer_id`); 
-ALTER TABLE `ims`.`orders` ADD FOREIGN KEY (`order_item_id`) REFERENCES `ims`.`orders_items` (`order_item_id`);
 ALTER TABLE `ims`.`orders_items` ADD FOREIGN KEY (`order_id`) REFERENCES `ims`.`orders` (`order_id`);
 ALTER TABLE `ims`.`orders_items` ADD FOREIGN KEY (`item_id`) REFERENCES `ims`.`items` (`item_id`);
