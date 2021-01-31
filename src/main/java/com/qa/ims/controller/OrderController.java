2a1833dba849d00dda3ac449a36df389d99407c3
TimothyOwen <towen@qa.com>
2021-01-31 17:31:54 +0000

    Integrated OrderController.java's create method with orderitems

--- src/main/java/com/qa/ims/controller/OrderController.java
+++ src/main/java/com/qa/ims/controller/OrderController.java
@@ -1,12 +1,15 @@
 package com.qa.ims.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import com.qa.ims.persistence.dao.OrderDAO;
+import com.qa.ims.persistence.dao.OrderItemDAO;
 import com.qa.ims.persistence.domain.Order;
+import com.qa.ims.persistence.domain.OrderItem;
 import com.qa.ims.utils.Utils;
 
 /**
@@ -18,11 +21,13 @@ public class OrderController implements CrudController<Order> {
 	public static final Logger LOGGER = LogManager.getLogger();
 
 	private OrderDAO orderDAO;
+	private OrderItemDAO orderitemDAO;
 	private Utils utils;
 
-	public OrderController(OrderDAO orderDAO, Utils utils) {
+	public OrderController(OrderDAO orderDAO, OrderItemDAO orderitemDAO, Utils utils) {
 		super();
 		this.orderDAO = orderDAO;
+		this.orderitemDAO = orderitemDAO;
 		this.utils = utils;
 	}
 
@@ -45,11 +50,39 @@ public class OrderController implements CrudController<Order> {
 	public Order create() {
 		LOGGER.info("Please enter a customer ID");
 		Long customer_id = utils.getLong();
+		// Calculate cost by referencing item ID
 		LOGGER.info("Please enter a cost of order");
 		Double cost = utils.getDouble();
 		LOGGER.info("Please enter a shipment date");
 		String shipment_date = utils.getString();
+		//
+		///
+		//
+		//Add try block here, return to here if invalid input
+		Order latestOrder = orderDAO.readLatest();
+		Long next_order_id = (long) 1;
+		if(latestOrder!=null) {
+			next_order_id = latestOrder.getOrderId()+1;
+		}
+		List<OrderItem> orderitems = new ArrayList<OrderItem>();
+		String user_finished = "N";
+		do {
+			LOGGER.info("Please enter an item ID");
+			Long item_id = utils.getLong();
+			LOGGER.info("Please enter how many items of these you want");
+			int item_quantity = Math.toIntExact(utils.getLong());
+			orderitems.add(new OrderItem(next_order_id, item_id, item_quantity));
+			LOGGER.info("Have you finished ordering? (Y/N)");
+			user_finished = utils.getString();
+		} while (user_finished.equals("N"));
 		Order order = orderDAO.create(new Order(customer_id, cost, shipment_date));
+		//
+		for(OrderItem orderitem: orderitems) {
+			orderitemDAO.create(orderitem);
+		}
+		//
+		///
+		//
 		LOGGER.info("Order created");
 		return order;
 	}
