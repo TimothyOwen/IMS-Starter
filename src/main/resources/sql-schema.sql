f56d161093ccdfc00afd245875753acfdfe789bb
TimothyOwen <towen@qa.com>
2021-02-01 16:33:03 +0000

    Updated schema

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -18,12 +18,11 @@ CREATE TABLE IF NOT EXISTS `ims`.`items` (
     PRIMARY KEY (`item_id`)
 );
 
-/*
 CREATE TABLE IF NOT EXISTS `ims`.`orders` (
     `order_id` INT(11) NOT NULL AUTO_INCREMENT,
     `customer_id` INT(11) DEFAULT NULL,
     `cost` DECIMAL(7,2) DEFAULT NULL,
-    `shipment_date` DECIMAL(7,2) DEFAULT NULL,
+    `shipment_date` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`order_id`)
 );
 
@@ -40,4 +39,6 @@ CREATE TABLE IF NOT EXISTS `ims`.`orders_items` (
 ALTER TABLE `ims`.`orders` ADD FOREIGN KEY (`customer_id`) REFERENCES `ims`.`customers` (`customer_id`); 
 ALTER TABLE `ims`.`orders_items` ADD FOREIGN KEY (`order_id`) REFERENCES `ims`.`orders` (`order_id`);
 ALTER TABLE `ims`.`orders_items` ADD FOREIGN KEY (`item_id`) REFERENCES `ims`.`items` (`item_id`);
-*/
+
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison'),('bill', 'harrison'),('jones', 'mcgee'),('steven', 'redgrave'),('willy', 'bower');
+INSERT INTO `ims`.`items` (`item_name`, `price`) VALUES ('beer', 1.99),('sprite', 1.99),('cola', 5.03),('vodka', 1.99),('wine', 2.01);
