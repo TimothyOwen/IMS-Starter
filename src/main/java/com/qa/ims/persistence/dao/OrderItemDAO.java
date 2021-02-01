afc5e8656d64deee8b2ca7180c71ed6d81184591
TimothyOwen <towen@qa.com>
2021-02-01 17:05:20 +0000

    Updated read order items method

--- src/main/java/com/qa/ims/persistence/dao/OrderItemDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/OrderItemDAO.java
@@ -3,7 +3,6 @@ package com.qa.ims.persistence.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
