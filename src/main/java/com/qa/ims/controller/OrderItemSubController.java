70158097e05cce184f4d20402c96fab6ab28523c
TimothyOwen <towen@qa.com>
2021-02-03 12:23:42 +0000

    Update order item sub controller

--- src/main/java/com/qa/ims/controller/OrderItemSubController.java
+++ src/main/java/com/qa/ims/controller/OrderItemSubController.java
@@ -1,5 +1,25 @@
 package com.qa.ims.controller;
 
-public class OrderItemSubController {
+import java.util.List;
 
+import com.qa.ims.persistence.dao.OrderItemDAO;
+import com.qa.ims.persistence.domain.OrderItem;
+import com.qa.ims.utils.Utils;
+
+public class OrderItemSubController {
+	private OrderItemDAO orderitemDAO;
+	private Utils utils;
+	public OrderItemSubController(OrderItemDAO orderitemDAO, Utils utils) {
+		super();
+		this.orderitemDAO = orderitemDAO;
+		this.utils = utils;
+	}
+	public void createOrderItems(List<OrderItem> orderitems) {
+		for (OrderItem orderitem : orderitems) {
+			orderitemDAO.create(orderitem);
+		}
+	}
+	public void deleteOrderItems(Long order_id) {
+		orderitemDAO.deleteByOrder(order_id);
+	}
 }
