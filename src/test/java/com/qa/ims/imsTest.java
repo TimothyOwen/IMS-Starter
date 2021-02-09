d99dfc4bfd6c5cd4f1fa0a03a64a1ec3cc62fc00
TimothyOwen <towen@qa.com>
2021-02-09 15:55:39 +0000

    Added IMS test

--- src/test/java/com/qa/ims/imsTest.java
+++ src/test/java/com/qa/ims/imsTest.java
@@ -1,11 +1,5 @@
 package com.qa.ims;
 
-import static org.junit.Assert.assertEquals;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -13,60 +7,63 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import com.qa.ims.controller.Action;
+import com.qa.ims.controller.CustomerAction;
 import com.qa.ims.controller.CustomerController;
 import com.qa.ims.controller.ItemController;
 import com.qa.ims.controller.OrderController;
-import com.qa.ims.controller.OrderItemSubController;
-import com.qa.ims.persistence.dao.CustomerDAO;
-import com.qa.ims.persistence.dao.ItemDAO;
-import com.qa.ims.persistence.dao.OrderDAO;
-import com.qa.ims.persistence.dao.OrderItemDAO;
 import com.qa.ims.persistence.domain.Access;
-import com.qa.ims.persistence.domain.Customer;
-import com.qa.ims.persistence.domain.CustomerDomain;
-import com.qa.ims.utils.DBUtils;
+import com.qa.ims.persistence.domain.Domain;
 import com.qa.ims.utils.Utils;
 
 @RunWith(MockitoJUnitRunner.class)
 public class imsTest {
 	
 	@Mock
-	private CustomerDAO customerDAO;
-	
-	@Mock
-	private ItemDAO itemDAO;
-	
-	@Mock
-	private OrderDAO orderDAO;
-	
-	@Mock
-	private OrderItemDAO orderitemDAO;
-	
-	@Mock
-	private CustomerController customerController;
-	
-	@Mock
-	private ItemController itemController;
-	
-	@Mock
-	private OrderController orderController;
-	
-	@Mock
-	private OrderItemSubController orderitemsubcontroller;
-	
-	@Mock
 	private Utils utils;
 	
+	@Mock
+	private CustomerController customers;
+	
+	@Mock
+	private ItemController items;
+	
+	@Mock
+	private OrderController orders;
+	
 	@InjectMocks
-	private IMS ims;
+	private IMS ims = new IMS();
 	
 	@Test
-	public void testSystem() {
-		ims = new IMS();
-		utils = ims.getUtils();
-		ims.imsCustomer();
-		Mockito.when(CustomerDomain.getCustomerDomain(utils)).thenReturn(CustomerDomain.valueOf("CUSTOMER"));
+	public void testSystemDeveloper() {
+		Access[] accesss = new Access[] {Access.CUSTOMER, Access.DEVELOPER, Access.EXIT};
+		Domain[] domains = new Domain[] {Domain.CUSTOMER, Domain.ITEM, Domain.ORDER, Domain.RETURN};
+		Action[] actions = new Action[] {Action.CREATE, Action.DELETE, Action.READ, Action.UPDATE, Action.RETURN};
+		Mockito.mockStatic(Access.class);
+		Mockito.mockStatic(Domain.class);
+		Mockito.mockStatic(Action.class);
+		this.utils = ims.getUtils();
+		Mockito.when(Access.getAccess(utils)).thenReturn(Access.DEVELOPER, Access.EXIT);
+		Mockito.when(Access.values()).thenReturn(accesss);
+		Mockito.when(Domain.getDomain(utils)).thenReturn(Domain.ORDER, Domain.RETURN);
+		Mockito.when(Domain.values()).thenReturn(domains);
+		Mockito.when(Action.getAction(utils)).thenReturn(Action.RETURN);
+		Mockito.when(Action.values()).thenReturn(actions);
+		ims.imsDeveloper();
 	}
 	
+	@Test
+	public void testSystemCustomer() {
+		Access[] accesss = new Access[] {Access.CUSTOMER, Access.DEVELOPER, Access.EXIT};
+		CustomerAction[] actions = new CustomerAction[] {CustomerAction.A, CustomerAction.B, CustomerAction.C, CustomerAction.D, CustomerAction.E, CustomerAction.F, CustomerAction.RETURN};
+		//Mockito.mockStatic(Access.class);
+		Mockito.mockStatic(CustomerAction.class);
+		this.utils = ims.getUtils();
+		Mockito.when(Access.getAccess(utils)).thenReturn(Access.CUSTOMER, Access.EXIT);
+		Mockito.when(Access.values()).thenReturn(accesss);
+		Mockito.when(CustomerAction.getAction(utils)).thenReturn(CustomerAction.RETURN);
+		Mockito.when(CustomerAction.values()).thenReturn(actions);
+		ims.imsSystem();
+	}
 	
 }
