2bd3c4719bd37ea1959e18580e6fa02a952d62a0
TimothyOwen <towen@qa.com>
2021-02-10 15:20:52 +0000

    Readded a modified ims test

--- src/test/java/com/qa/ims/IMSTest.java
+++ src/test/java/com/qa/ims/IMSTest.java
@@ -1,11 +1,7 @@
 package com.qa.ims;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 
 import com.qa.ims.controller.Action;
 import com.qa.ims.controller.CustomerAction;
@@ -13,14 +9,13 @@ import com.qa.ims.persistence.domain.Access;
 import com.qa.ims.persistence.domain.Domain;
 import com.qa.ims.utils.Utils;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest(IMS.class)
 public class IMSTest {
-	/*
+	private Utils utils;
+	private IMS ims = new IMS();
 	@Test
-	public void testSystemDeveloper() {
+	public void testSystemAdministrator() {
 		IMS ims = new IMS();
-		Utils utils = new Utils();
+		this.utils = ims.getUtils();
 		Access[] accesss = new Access[] {Access.CUSTOMER, Access.ADMINISTRATOR, Access.EXIT};
 		Domain[] domains = new Domain[] {Domain.CUSTOMER, Domain.ITEM, Domain.ORDER, Domain.RETURN};
 		Action[] actions = new Action[] {Action.CREATE, Action.DELETE, Action.READ, Action.UPDATE, Action.RETURN};
@@ -33,14 +28,12 @@ public class IMSTest {
 		Mockito.when(Domain.values()).thenReturn(domains);
 		Mockito.when(Action.getAction(utils)).thenReturn(Action.RETURN);
 		Mockito.when(Action.values()).thenReturn(actions);
-		PowerMockito.verifyStatic();
 		ims.imsAdministrator();
 	}
-	*/
+	
 	@Test
 	public void testSystemCustomer() {
-		IMS ims = new IMS();
-		Utils utils = new Utils();
+		this.utils = ims.getUtils();
 		Access[] accesss = new Access[] {Access.CUSTOMER, Access.ADMINISTRATOR, Access.EXIT};
 		CustomerAction[] actions = new CustomerAction[] {CustomerAction.A, CustomerAction.B, CustomerAction.C, CustomerAction.D, CustomerAction.E, CustomerAction.F, CustomerAction.RETURN};
 		Mockito.mockStatic(CustomerAction.class);
@@ -48,7 +41,6 @@ public class IMSTest {
 		Mockito.when(Access.values()).thenReturn(accesss);
 		Mockito.when(CustomerAction.getAction(utils)).thenReturn(CustomerAction.RETURN);
 		Mockito.when(CustomerAction.values()).thenReturn(actions);
-		PowerMockito.verifyStatic();
 		ims.imsSystem();
 	}
 }
